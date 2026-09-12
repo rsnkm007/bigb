b73159553a7e7995499ad65cebf27778c7bdc59f
nanda kumar <79748921+rsnkm007@users.noreply.github.com>
2026-09-12 15:20:26 +0530

    Configure frontend API for Render

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const adminApi = axios.create({
-
-    baseURL: "http://localhost:5000/api"
-
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 export default adminApi;
--- src/api/orderApi.js
+++ src/api/orderApi.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const orderApi = axios.create({
-
-    baseURL: "http://localhost:5000/api"
-
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 export default orderApi;
